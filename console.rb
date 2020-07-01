9e9c5ae4f7acc07135211be79f1f60a18f7a95cc
Rebeka Geczi <geczirebeka@gmail.com>
2020-07-01 14:51:58 +0100

    album save

--- console.rb
+++ console.rb
@@ -1,10 +1,18 @@
 require('pry-byebug')
 require_relative('models/artist')
+require_relative('models/album')
 
 artist1 = Artist.new({
     'name' => 'Rihanna'
 })
 artist1.save()
+
+album1 = Album.new({
+    'title' => 'Loud',
+    'genre' => 'pop',
+    'artist_id' => artist1.id
+})
+album1.save()
 
 
 
